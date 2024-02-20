edd9eba1e8f04b3ba3075221509250fb5847ad0c
shegol2020 <shegol2007@gmail.com>
2024-02-20 13:24:44 +0300

    feat: adding visible cards onclick

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {useState} from "react";
 //import {finishedItems, visibleItems} from "./data";
 
 function App({images}) {
@@ -22,25 +23,34 @@ const GameContainer = styled.section`
 `
 
 function CardContainer({images}){
-    const visibleItems = ['hX_hf2lPpUU', '3tYZjGSBwbk'];
-    const finishedItems = ['YdAqiUkUoWA', 'YdAqiUkUoWA1', 'w1JE5duY62M', 'w1JE5duY62M1'];
+    const [visibleItems, setVisibleItems] = useState(['hX_hf2lPpUU', '3tYZjGSBwbk']);
+    const [finishedItems, setFinishedItems] = useState(['YdAqiUkUoWA', 'YdAqiUkUoWA1', 'w1JE5duY62M', 'w1JE5duY62M1']);
+    const cardClickHandler = (id) => {
+        setVisibleItems(items => [...items, id]);
+    }
     return <>
         <ul className="cards cards-theme-cars">
             {images.map(item => <Card
                 key={item.id}
+                id={item.id}
                 url={item.url}
                 description={item.description}
                 isShown={visibleItems.includes(item.id)}
                 isFinished={finishedItems.includes(item.id)}
+                cardClick={cardClickHandler}
             />)}
         </ul>
     </>
 }
 
-function Card({url, description, isShown, isFinished}){
+function Card({url, description, isShown, isFinished, cardClick, id}){
     const className = `card ${isShown ? 'card-show' : ''} ${isFinished ? 'card-finished' : ''}`;
+    const handleClick = () => {
+        if(isShown || isFinished){ return }
+        cardClick(id)
+    }
     return <>
-        <li className={className}>
+        <li className={className} onClick={handleClick}>
             <img src={url} width="204" height="144" alt={description}/>
         </li>
     </>
